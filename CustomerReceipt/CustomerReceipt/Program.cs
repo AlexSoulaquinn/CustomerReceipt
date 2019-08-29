4e81df6651bbb6ab2e710bf9631d3a94b3dadfba
Alex Quinn <Alex.Quinn@ou.edu>
2019-08-29 11:26:21 -0500

    Finished code

---
+++ CustomerReceipt/CustomerReceipt/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerReceipt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declare variables (reserving memory so that we have places to store our values)
+            const double cSalesTax = 0.086;//Creates a constant with the value of 8.6% and keeps it from changing.
+            double salesTax;//Can declare variables one at a time
+            double netTotal;
+            double totalAmount;
+            string name, salesTaxAsString, netTotalAsString; // or you can declare variables with the same datatype all on one line
+
+            // Get input from the user
+            Console.WriteLine("Please enter your name. >>");//Write to the console asking the user for their name
+            name = Console.ReadLine();//Read the response from the user and store it in a variable
+
+            Console.WriteLine("Enter sales tax (as a decimal) >>");
+            salesTaxAsString = Console.ReadLine();
+
+            Console.WriteLine("Enter your net total >>");
+            netTotalAsString = Console.ReadLine();
+
+            // Convert strings to number data types
+            salesTax = Convert.ToDouble(salesTaxAsString);
+            netTotal = Convert.ToDouble(netTotalAsString);
+
+            double taxTotal = salesTax * netTotal; // declare a variable and assign a value at once
+            totalAmount = netTotal + taxTotal;
+
+            string sentence = name + " with a sales tax of " + salesTax.ToString("P") +
+                              " you will pay " + taxTotal.ToString("C2") +
+                              " in tax making your grand total to be " + totalAmount.ToString("C"); //Concatenate a bunch of strings together into a single one
+            // Output
+            Console.WriteLine(sentence); //output our string to the user
+            /*Can write a single statement to concatenate and output in one step
+             * Console.WriteLine(name + " with a sales tax of " + salesTax.ToString("P") +
+                              " you will pay " + taxTotal.ToString("C2") +
+                              " in tax making your grand total to be " + totalAmount.ToString("C"));*/
+            Console.ReadKey();//Stopping point so the application doesn't close
+        }
+    }
+}
